77bf48ccfc7f7f83b1ca4f8d879df09a6a0381f7
Lucas França <lucas.rfranca@gmail.com>
2022-10-23 16:30:18 -0300

    apresenta todos os números divisíveis por 5 que sejam menores que 100

---
+++ divisivel_5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Apresentar na linguagem C todos os números 
+divisíveis por 5 que sejam menores que 100. */
+
+main() {
+	
+	int i=5;
+	
+	while (i<100) {
+		
+		printf ("%d ", i);
+		
+		i+=5;
+		
+	}
+	
+}
